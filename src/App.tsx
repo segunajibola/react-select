ad84d4b8349f29dab45db0015578f94a0d0cdf6c
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-31 13:31:44 +0100

    firstCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { Select } from "./Select";
 // import "./App.css";
 
 function App() {
 
-  return <div className="App">
-    <h1>Hi</h1>
-  </div>;
+  const options = [
+    { label: "First", value: 1 },
+    { label: "Second", value: 2 },
+    { label: "Third", value: 3 },
+    { label: "Fourth", value: 4 },
+    { label: "Fifth", value: 5 },
+  ]
+  return (
+    <div className="App">
+      <Select />
+    </div>
+  );
 }
+
+// value={"hello", "hvb"} options={"hello"} onChange={() => (3)}
 
 export default App;
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./select.module.css";
 
 type SelectOption = {
-    label: string
-    value: string
-}
+  label: string;
+  value: any;
+};
 
 type SelectProps = {
-    options: (value: SelectOption | undefined) => void
-    value: SelectOption[]
-    onChange: SelectOption
-}
+  options: SelectOption[]; // array of different options i.e label & value
+  value?: SelectOption; // one of those options i.e label and value
+  onChange: (value: SelectOption | undefined) => void; // arrow func, value as parameter, value is either a label type of value type i.e string or any (any type)
+};
 
 export function Select({ value, onChange, options }: SelectProps) {
   return (
-    <div>Select</div>
-  )
+    <>
+      <div className="container">
+        <span className={styles.value}>Value</span>
+        <button className={styles["clear-btn"]}>&times;</button>
+        <div className={styles.divider}></div>
+        <div className={styles.caret}></div>
+        <ul className={styles.optiond}>
+            {options.map(option => (
+                <li key={option.label} className={styles.option}>{option.label}</li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
 }
